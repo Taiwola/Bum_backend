b9f392529cb3b921c18b139fd06e4c991ba33b71
Taiwola <seunolanitori@gmail.com>
2024-06-13 09:39:00 +0100

    more adjustments

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/', getAllUsers);
 router.get('/:id', getOneUser);
 router.get("/team/:Id", authentication, getTeamMembers);
-router.put('/:id', updateUser);
+router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
 
 export {router as userRouter};
